d1a70ba3de44bc502b2a8a16375e38c9aa2afd82
Prasoon Parashar <parasharprasoon.947@gmail.com>
2023-09-11 09:01:50 +0530

    Converted to Function

--- src/main/scala/com/its/ecartsales/services/decryptor/GetPasswordFromSecretManager.scala
+++ src/main/scala/com/its/ecartsales/services/decryptor/GetPasswordFromSecretManager.scala
@@ -13,53 +13,88 @@ import java.nio.ByteBuffer
 
 object GetPasswordFromSecretManager extends App {
 
- val region = Regions.US_EAST_1
-
+ lazy val region = Regions.US_EAST_1
 
  val client: AWSSecretsManager = AWSSecretsManagerClientBuilder.standard()
    .withRegion(region)
    .build()
- val getSecretValueRequest = new GetSecretValueRequest().withSecretId("dev/XYZ_DATABASE")
- val getSecretValueResult: GetSecretValueResult = client.getSecretValue(getSecretValueRequest)
+ def getAndDecryptSecret(secretId: String, kmsKeyId: String): String = {
+  val getSecretValueRequest = new GetSecretValueRequest ().withSecretId (secretId)
+  val getSecretValueResult: GetSecretValueResult = client.getSecretValue (getSecretValueRequest)
+  println ("Secret retrieved ")
+  val secretBinaryString = getSecretValueResult.getSecretString
+  print (secretBinaryString)
 
- println("Secret retrieved ")
- val secretBinaryString = getSecretValueResult.getSecretString
- print(secretBinaryString)
+  val objectMapper = new ObjectMapper ()
 
-
-
- val objectMapper = new ObjectMapper()
-
- val secretMap = objectMapper.readValue(secretBinaryString, classOf[java.util.HashMap[String, String]])
+  val secretMap = objectMapper.readValue (secretBinaryString, classOf[java.util.HashMap[String, String]] )
 
 
- val inputBase64EncryptedData= secretMap.get("XYZ_DATABASE_PASSWORD")
- //println("Secret password = " + secretMap.get("password"))
- val kmsClient = AWSKMSClientBuilder.standard()
-   .withRegion(Regions.US_EAST_1)
-   .build()
+  val inputBase64EncryptedData = secretMap.get ("XYZ_DATABASE_PASSWORD")
+  //println("Secret password = " + secretMap.get("password"))
+  val kmsClient = AWSKMSClientBuilder.standard ()
+ .withRegion (Regions.US_EAST_1)
+ .build ()
 
 
- println("Enter the Base64-encoded encrypted data:")
-// val inputBase64EncryptedData = scala.io.StdIn.readLine()
+  // Decode the Base64-encoded string to a byte array
+  val encryptedData1: Array[Byte] = Base64.getDecoder.decode (inputBase64EncryptedData)
 
- // Decode the Base64-encoded string to a byte array
- val encryptedData1: Array[Byte] = Base64.getDecoder.decode(inputBase64EncryptedData)
+  // Decrypt using AWS KMS
+  val decryptRequest = new com.amazonaws.services.kms.model.DecryptRequest ()
+ .withKeyId (sys.env (kmsKeyId) )
+ .withCiphertextBlob (ByteBuffer.wrap (encryptedData1) )
 
- // Decrypt using AWS KMS
- val decryptRequest1 = new com.amazonaws.services.kms.model.DecryptRequest()
-   .withKeyId(sys.env("CUSTOMER_MASTER_KEY"))
-   .withCiphertextBlob(ByteBuffer.wrap(encryptedData1))
+  val decryptResponse1 = kmsClient.decrypt (decryptRequest)
+  val decryptedData1 = decryptResponse1.getPlaintext.array ()
 
- val decryptResponse1 = kmsClient.decrypt(decryptRequest1)
- val decryptedData1 = decryptResponse1.getPlaintext.array()
+  // Display the decrypted data
+  val decryptedString1 = new String (decryptedData1)
+  println ("Decrypted Data: " + decryptedString1)
+  decryptedString1
 
- // Display the decrypted data
- val decryptedString1 = new String(decryptedData1)
- println("Decrypted Data: " + decryptedString1)
+  // val url = s"jdbc:postgresql://${secretMap.get("host")}:${secretMap.get("port")}/dbName"
+  // println("Secret url = " + url)
+  // println("Secret username = " + secretMap.get("username"))
+  // println("Secret password = " + secretMap.get("password"))
 
-// val url = s"jdbc:postgresql://${secretMap.get("host")}:${secretMap.get("port")}/dbName"
-// println("Secret url = " + url)
-// println("Secret username = " + secretMap.get("username"))
-// println("Secret password = " + secretMap.get("password"))
+ }
+// val getSecretValueRequest = new GetSecretValueRequest().withSecretId("dev/XYZ_DATABASE")
+// val getSecretValueResult: GetSecretValueResult = client.getSecretValue(getSecretValueRequest)
+//
+// println("Secret retrieved ")
+// val secretBinaryString = getSecretValueResult.getSecretString
+// print(secretBinaryString)
+//
+// val objectMapper = new ObjectMapper()
+//
+// val secretMap = objectMapper.readValue(secretBinaryString, classOf[java.util.HashMap[String, String]])
+//
+//
+// val inputBase64EncryptedData= secretMap.get("XYZ_DATABASE_PASSWORD")
+// //println("Secret password = " + secretMap.get("password"))
+// val kmsClient = AWSKMSClientBuilder.standard()
+//   .withRegion(Regions.US_EAST_1)
+//   .build()
+//
+//
+// // Decode the Base64-encoded string to a byte array
+// val encryptedData1: Array[Byte] = Base64.getDecoder.decode(inputBase64EncryptedData)
+//
+// // Decrypt using AWS KMS
+// val decryptRequest = new com.amazonaws.services.kms.model.DecryptRequest()
+//   .withKeyId(sys.env("CUSTOMER_MASTER_KEY"))
+//   .withCiphertextBlob(ByteBuffer.wrap(encryptedData1))
+//
+// val decryptResponse1 = kmsClient.decrypt(decryptRequest)
+// val decryptedData1 = decryptResponse1.getPlaintext.array()
+//
+// // Display the decrypted data
+// val decryptedString1 = new String(decryptedData1)
+// println("Decrypted Data: " + decryptedString1)
+//
+//// val url = s"jdbc:postgresql://${secretMap.get("host")}:${secretMap.get("port")}/dbName"
+//// println("Secret url = " + url)
+//// println("Secret username = " + secretMap.get("username"))
+//// println("Secret password = " + secretMap.get("password"))
 }
--- src/main/scala/com/its/ecartsales/services/decryptor/ReadEnvironmentVariable.scala
+++ src/main/scala/com/its/ecartsales/services/decryptor/ReadEnvironmentVariable.scala
@@ -2,10 +2,13 @@ package com.its.ecartsales.services.decryptor
 
 import com.its.ecartsales.framework.jobs.Job
 import org.apache.spark.sql.SparkSession
+import com.its.ecartsales.services.decryptor.GetPasswordFromSecretManager._
 
 class ReadEnvironmentVariable (spark: SparkSession) extends Job{
-  println("Hellloooooo")
-  println("I am from line 8")
-  println("I am from system variable",sys.env("CUSTOMER_MASTER_KEY"))
+
+  lazy val secretId = "dev/XYZ_DATABASE"
+  lazy val CUSTOMER_MASTER_KEY =sys.env("CUSTOMER_MASTER_KEY")
+  val myDbPasswd = getAndDecryptSecret(secretId,CUSTOMER_MASTER_KEY)
+  println(myDbPasswd)
 
 }
